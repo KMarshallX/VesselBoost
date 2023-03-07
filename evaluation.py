756f553496c31b69bb5285e516ec6a1f41e852a3
marshallxu <kmarshallxu@gmail.com>
2023-03-07 12:51:21 +1000

    update evaluate metrics

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,7 @@
 """
 5 Evaluation metrics used to evaluate the resulting segmentation
 
-Marshall @ 03/01/2023
+Marshall @ 03/07/2023
 """
 
 import numpy as np
@@ -9,24 +9,30 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, normalized_mutual_info_score
 import nibabel as nib
 
-class eval_radar:
+class eval_scores:
     def __init__(self, seg, gt) -> None:
         """
         seg, gt: numpy array
         """
         self.seg = seg.reshape(-1,1)
-        self.gt = gt.reshape(-1,1) 
-
-        self.tn, self.fp, self.fn, self.tp = confusion_matrix(self.gt, self.seg).ravel()
+        self.gt = gt.reshape(-1,1)
+        self.tn, self.fp, self.fn, self.tp = self._four_cardinalities(self.seg, self.gt)
         self.n = self.tn + self.fp + self.fn + self.tp
+
+    def _four_cardinalities(self, seg, gt):
+        tp = (seg*gt).sum()
+        fp = (seg*(1-gt)).sum()
+        fn = ((1-seg)*gt).sum()
+        tn = ((1-seg)*(1-gt)).sum()
+        return tn, fp, fn, tp
     
     def _dice(self):
         # dice score
-        return (2*np.sum(self.seg * self.gt)) / (np.sum(self.seg) + np.sum(self.gt))
+        return (2*self.tp) / (2*self.tp + self.fp + self.fn)
     
     def _jaccard(self):
         # jaccard score
-        return (np.sum(self.seg * self.gt)) / (np.sum(np.logical_or(self.seg, self.gt)))
+        return self.tp / (self.tp + self.fp + self.fn)
     
     def _vol_sim(self):
         # volume similarity
@@ -42,8 +48,9 @@ class eval_radar:
         Hsg = -(psg1*np.log(psg1) + psg2*np.log(psg2))
         Hst = -(pst1*np.log(pst1) + pst2*np.log(pst2))
         Hsgst = -((self.tp/self.n)*np.log(self.tp/self.n) + (self.fn/self.n)*np.log(self.fn/self.n) + (self.fp/self.n)*np.log(self.fp/self.n) + (self.tn/self.n)*np.log(self.tn/self.n))
+        MI = Hsg + Hst - Hsgst
 
-        return Hsg + Hst - Hsgst
+        return 2*MI / (Hsg + Hst)
     
     def _norm_mi(self):
         # normalized mutual information
@@ -60,62 +67,54 @@ class eval_radar:
 
         return 2*(a*d - b*c) / (c**2 + b**2 + 2*a*d + (a+d)*(c+b))
     
-    def __call__(self, pr=False):
+    def __call__(self):
         a = self._dice()
         b = self._jaccard()
         c = self._vol_sim()
-        d = self._norm_mi()
+        d = self._mutual_info()
         e = self._adjusted_ri()
-        categories = ['Dice', 'Jaccard', 'Vol_Sim', 'Norm_MI', 'ARI']
-        val_list = [a,b,c,d,e]
-        val_list += val_list[:1]
-
-        angles = [n / float(5) * 2 * np.pi for n in range(5)]
-        angles += angles[:1]
-
-        if pr == True:
-            print(f"Dice score: {a}\n")
-            print(f"Jaccard score: {b}\n")
-            print(f"Volume Similarity: {c}\n")
-            print(f"Mutual Information: {d}\n")
-            print(f"Adjusted Rand Index: {e}\n")
-
-        # Initialise the spider plot
-        plt.figure()
-        ax = plt.subplot(111, polar=True)
-        # Draw one axe per variable + add labels
-        plt.xticks(angles[:-1], categories, color='black', size=8)
-        # Draw ylabels
-        ax.set_rlabel_position(0)
-        plt.yticks([0.2,0.4,0.6,0.8], ["0.2","0.4","0.6","0.8"], color="grey", size=7)
-        plt.ylim(0,1)
         
-        
-        # Plot data
-        ax.plot(angles, val_list, linewidth=1, linestyle='solid')
-        # Fill area
-        ax.fill(angles, val_list, 'b', alpha=0.1)
-        # Show the graph
-        plt.show()
-
-
-        return
+        return [a,b,c,d,e]
 
 if __name__ == "__main__":
 
-    test_seg_path = "./saved_image/week11/postpro_15_2.nii.gz" 
-    test_seg = nib.load(test_seg_path).get_fdata()
+    ground_truth_path = "./data/validate_label/sub017.nii"
+    ground_truth_img = nib.load(ground_truth_path)
+    ground_truth = ground_truth_img.get_fdata()
 
-    test_data_path = "./data/all_label/15.nii"
-    test_img = nib.load(test_data_path)
-    ground_truth = test_img.get_fdata()
+    saved_img_path = "./saved_image/"
+    eval_img_list = [saved_img_path+"week13/val_re_unet_ep5000_bce_dice_17.nii.gz", saved_img_path+"week13/val_re_unet_ep5000_dice_bce_17.nii.gz", saved_img_path+"week13/val_re_unet_ep5000_dice_tver_17.nii.gz"]
 
-    omlette_path = "./data/train_OMELETTElabel/sub015_seg_g0.02.nii.gz"
-    omlette_seg = nib.load(omlette_path).get_fdata()
+    vals_list = []
+    for i in range(len(eval_img_list)):
+        out_img = nib.load(eval_img_list[i]).get_fdata()
+        scores_itm = eval_scores(out_img, ground_truth.round())
+        vals = scores_itm()
+        print(vals)
+        vals += vals[:1]
+        vals_list.append(vals)
 
-    plausible_path = "./data/train_plausiblelabel/sub015_Segmentation_9.nii.gz"
-    plausible_seg = nib.load(plausible_path).get_fdata()
+    categories = ['Dice', 'Jaccard', 'Vol_Sim', 'Norm_MI', 'ARI']
+    angles = [n / float(5) * 2 * np.pi for n in range(5)]
+    angles += angles[:1]
+    label_list = ["bce_dice", "dice_bce", "dice_tver"]
+    # Initialise the spider plot
+    plt.figure()
+    ax = plt.subplot(111, polar=True)
+    # Draw one axe per variable + add labels
+    plt.xticks(angles[:-1], categories, color='black', size=8)
+    # Draw ylabels
+    ax.set_rlabel_position(0)
+    plt.yticks([0.2,0.4,0.6,0.8], ["0.2","0.4","0.6","0.8"], color="grey", size=7)
+    plt.ylim(0,1)
+    
+    # Plot data
+    for j in range(len(eval_img_list)):
+        ax.plot(angles, vals_list[j], color=f"C{j}", linewidth=1, linestyle='solid', label=label_list[j])
+        # Fill area
+        ax.fill(angles, vals_list[j], color=f"C{j}", alpha=0.1)
+    
+    # Show the graph
+    plt.legend()
+    plt.show()
 
-    radar = eval_radar(test_seg, ground_truth.round())
-
-    radar(pr=True)
