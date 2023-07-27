e08babbb6cb89c8f212f5f554c225306c2780d44
Fernanda Ribeiro <felenita.ribeiro@gmail.com>
2023-07-27 11:52:22 +1000

    remove osfcli.config

--- tests/test_osfintegration.sh
+++ tests/test_osfintegration.sh
@@ -7,9 +7,9 @@ export OSF_TOKEN=$OSF_TOKEN_
 export OSF_USERNAME=$OSF_USERNAME_
 export OSF_PROJECT_ID=$OSF_PROJECT_ID_
 echo $OSF_PROJECT_ID
-mkdir -p ~/.osfcli
-echo -e "[osf]\nproject = $OSF_PROJECT_ID\nusername = $OSF_USERNAME" > ~/.osfcli/osfcli.config
-cat ~/.osfcli/osfcli.config
+# mkdir -p ~/.osfcli
+# echo -e "[osf]\nproject = $OSF_PROJECT_ID\nusername = $OSF_USERNAME" > ~/.osfcli/osfcli.config
+# cat ~/.osfcli/osfcli.config
 ls
 export timestamp=$(date +%Y%m%d_%H%M%S)
 echo $timestamp
