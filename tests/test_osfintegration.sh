598e2eacfab3cd3400c8e2a349b927c5c89013b9
Fernanda Ribeiro <felenita.ribeiro@gmail.com>
2023-07-27 12:50:43 +1000

    minor edit

--- tests/test_osfintegration.sh
+++ tests/test_osfintegration.sh
@@ -15,4 +15,4 @@ export timestamp=$(date +%Y%m%d_%H%M%S)
 echo $timestamp
 export file="figure1_v1.png "
 echo "$timestamp"_"$file"
-osf -p abk4p upload -r ./paper/ /osfstorage/test/
+osf -p abk4p upload ./paper/$file /osfstorage/test/$file
